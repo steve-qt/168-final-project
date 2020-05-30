3c3daf8b89bb8f8e72e778a25204a2dd7ef18a6e
Xiangkun Zhao <xiangkunzhao@Xiangkuns-MacBook-Pro.local>
2020-05-30 09:08:59 -0700

    split training and testing data from batch

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import os
 import numpy as np
 from sklearn import tree
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import KFold
+from sklearn.model_selection import RepeatedKFold
 
 def main():
     # 1.import non pwi
@@ -14,31 +16,31 @@ def main():
     case_arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
     # 2.import pwi by case
-    # for item in case_arr:
-    #     nii.import_pwi_by_case(item)
+    #for item in case_arr:
+       # nii.import_pwi_by_case(item)
 
     # 3.get training set and save it
-    # batch_name = "1-10"
-    # if not os.path.isdir("batch"):
-    #     os.makedirs("batch")
-    # if not os.path.isdir("key"):
-    #     os.makedirs("key")
+    #batch_name = "1-10"
+    #if not os.path.isdir("batch"):
+      #os.makedirs("batch")
+    #if not os.path.isdir("key"):
+        # os.makedirs("key")
     #
-    # file_name = os.path.join("batch",batch_name)
-    # key_path = os.path.join("key",batch_name)
+    #file_name = os.path.join("batch",batch_name)
+    #key_path = os.path.join("key",batch_name)
     #
-    # for case_id in case_arr:
-    #     slice_id = 0
-    #     batch,key = trainer.get_training_set(case_id, slice_id)
-    #     with open(file_name,"w+") as f:
-    #         np.savetxt(f,batch,fmt="%.6f")
-    #     with open(key_path,"w+") as g:
-    #         np.savetxt(g,key,fmt="%d")
-    #     f.close()
-    #     g.close()
+    #for case_id in case_arr:
+        #slice_id = 0
+        #batch,key = trainer.get_training_set(case_id, slice_id)
+        #with open(file_name,"w+") as f:
+            #np.savetxt(f,batch,fmt="%.6f")
+        #with open(key_path,"w+") as g:
+            #np.savetxt(g,key,fmt="%d")
+        #f.close()
+        #g.close()
 
     # 4. testing dataset
-    nii.import_pwi_by_case(12)
+    #nii.import_pwi_by_case(12)
     batch, key = trainer.get_training_set(12, 1)
     with open("batch/12", "w+") as f:
         np.savetxt(f, batch, fmt="%.6f")
@@ -46,6 +48,24 @@ def main():
         np.savetxt(g, key, fmt="%d")
     f.close()
     g.close()
+    # 4.5.  K FOlD split some test data from  training data
+
+    #split = 5, train= 80%, test =20%
+    rpkf=RepeatedKFold(n_splits=5, n_repeats=50, random_state=2652124) #repeat 50 times, adds more accuracy
+    rpkf.get_n_splits(batch)
+    print(rpkf)
+    training_data=[]
+    testing_data=[]
+    for train_index, test_index in rpkf.split(batch):
+        #print("TRAIN:", train_index, "TEST:", test_index)
+        #print("TEST:", test_index)
+        training_data.append(train_index)
+        testing_data.append(test_index)
+
+    print(testing_data)
+    #training_data,testing_data
+
+
 
     # 5.feed to decision tree machine learning
     X = np.loadtxt(os.path.join("batch","1-10"))
@@ -57,7 +77,7 @@ def main():
     Y_test = np.loadtxt(os.path.join("key", "12"))
     Y_predicted = clf.predict(X_test)
 
-    print(Y_predicted)
+    #print(Y_predicted)
     score = accuracy_score(Y_test, Y_predicted)
     print("Accuracy score = ",score)
 if __name__ == "__main__":
